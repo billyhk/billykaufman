3a9faab7ed1e27e9fc68e7d53063797449cf2c30
billyh <billyhkaufman@outlook.com>
2020-08-12 18:45:19 -0400

    added react-grid-gallery for projects

---
+++ src/components/ProjectGalleryImages.js
@@ -0,0 +1,38 @@
+// JSPECT
+import jspect1 from '../component-images/project-images/jspect/jspect-home.png';
+import jspect2 from '../component-images/project-images/jspect/jspect-works.png';
+import jspect3 from '../component-images/project-images/jspect/jspect-detail.png';
+import jspect4 from '../component-images/project-images/jspect/jspect-form.png';
+import jspect5 from '../component-images/project-images/jspect/jspect-table.png';
+
+// STATEMENTS
+import statements1 from '../component-images/project-images/statements/statements-home.png';
+import statements2 from '../component-images/project-images/statements/statements-detail.png';
+import statements3 from '../component-images/project-images/statements/statements-all.png';
+import statements4 from '../component-images/project-images/statements/statements-statements.png';
+
+// RECIPE SEARCHER
+import recipe1 from '../component-images/project-images/recipe-searcher/recipe-home.png';
+import recipe2 from '../component-images/project-images/recipe-searcher/recipe-detail.png';
+
+// SIMON
+import simon1 from '../component-images/project-images/simon/simon-new-game.png';
+import simon2 from '../component-images/project-images/simon/simon-win-game.png';
+
+export const GalleryImages = {
+	jspect: [
+		{ src: jspect1, thumbnail: jspect1 },
+		{ src: jspect2 },
+		{ src: jspect3 },
+		{ src: jspect4 },
+		{ src: jspect5 },
+	],
+	statements: [
+		{ src: statements1, thumbnail: statements1 },
+		{ src: statements2 },
+		{ src: statements3 },
+		{ src: statements4 },
+	],
+	recipe: [{ src: recipe1, thumbnail: recipe1 }, { src: recipe2 }],
+	simon: [{ src: simon1, thumbnail: simon1 }, { src: simon2 }],
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,178 +1,227 @@
 import React from 'react';
+import { GalleryImages } from './ProjectGalleryImages';
+import Gallery from 'react-grid-gallery';
 
 // import local images
-import statements from '../component-images/statements.png';
-import fire from '../component-images/fire.png';
-import recipes from '../component-images/recipe-searcher.png';
-import simon from '../component-images/simon.png';
+// import statements from '../component-images/statements.png';
+// import fire from '../component-images/fire.png';
+// import recipes from '../component-images/recipe-searcher.png';
+// import simon from '../component-images/simon.png';
 
 const Projects = () => (
-		<main className='about-main'>
-			<h1 className='about-heading'>My Projects</h1>
+	<main className='about-main'>
+		<h1 className='about-heading'>
+			My Projects
+			<br />
+			<small>(Click on images to view more)</small>
+		</h1>
+		<h2 className='about-heading2'>Joel Spector</h2>
+		<div className='project-container'>
+			{/* <img alt='statements-financial-statement' src={statements} width='100%' /> */}
+			<Gallery images={GalleryImages['jspect']} enableImageSelection={false} />{' '}
+			<ul className='project-description-list'>
+				<section>
+					{/* <li>
+						<a
+							href='https://statements-frontend.herokuapp.com'
+							target='blank'
+							rel='noopener noreferrer'>
+							Deployed URL
+						</a>
+					</li> */}
+					<li>
+						<a
+							href='https://github.com/billyhk/joel-spector-frontend'
+							target='blank'
+							rel='noopener noreferrer'>
+							Github (front-end)
+						</a>
+					</li>
+					<li>
+						<a
+							href='https://github.com/spectorari/jspect-backend'
+							target='blank'
+							rel='noopener noreferrer'>
+							Github (back-end)
+						</a>
+					</li>
+				</section>
+				<section>
+					<li>Description:</li>
+					<ul className='project-sublist'>
+						<li>React/CSS/Material-Design-Bootstrap</li>
+						<li>Java Spring Boot / MySQL</li>
+						<li>Authentication with Spring Security / JWT</li>
+					</ul>
+				</section>
+			</ul>
+		</div>
 
-			<h2 className='about-heading2'>Statements</h2>
-			<div className='project-container'>
-				<img
-					alt='statements-financial-statement'
-					src={statements}
-					width='100%'
-				/>
-				<ul className='project-description-list'>
-					<section>
-						<li>
-							<a
-								href='https://statements-frontend.herokuapp.com'
-								target='blank'
-								rel='noopener noreferrer'>
-								Deployed URL
-							</a>
-						</li>
-						<li>
-							<a
-								href='https://github.com/billyhk/statements_frontend/blob/master/README.md'
-								target='blank'
-								rel='noopener noreferrer'>
-								Github (front-end)
-							</a>
-						</li>
-						<li>
-							<a
-								href='https://github.com/trental/statements_backend'
-								target='blank'
-								rel='noopener noreferrer'>
-								Github (back-end)
-							</a>
-						</li>
-					</section>
-					<section>
+		<h2 className='about-heading2'>Statements</h2>
+		<div className='project-container'>
+			{/* <img alt='statements-financial-statement' src={statements} width='100%' /> */}
+			<Gallery
+				images={GalleryImages['statements']}
+				enableImageSelection={false}
+			/>{' '}
+			<ul className='project-description-list'>
+				<section>
+					<li>
+						<a
+							href='https://statements-frontend.herokuapp.com'
+							target='blank'
+							rel='noopener noreferrer'>
+							Deployed URL
+						</a>
+					</li>
+					<li>
+						<a
+							href='https://github.com/billyhk/statements_frontend/blob/master/README.md'
+							target='blank'
+							rel='noopener noreferrer'>
+							Github (front-end)
+						</a>
+					</li>
+					<li>
+						<a
+							href='https://github.com/trental/statements_backend'
+							target='blank'
+							rel='noopener noreferrer'>
+							Github (back-end)
+						</a>
+					</li>
+				</section>
+				<section>
+					<li>Description:</li>
+					<ul className='project-sublist'>
+						<li>React/CSS/Bootstrap-React</li>
+						<li>Python/Django/PostgresSQL</li>
+						<li>Authentication with JWT</li>
+					</ul>
+				</section>
+			</ul>
+		</div>
+		{/* <h2 className='about-heading2'>f i r e</h2>
+
+		<div className='project-container'>
+			<img alt='fire-home' src={fire} width='100%' />
+			<ul className='project-description-list'>
+				<section>
+					<li>
+						<a
+							href='https://fire-explorer-frontend.herokuapp.com'
+							target='blank'
+							rel='noopener noreferrer'>
+							Deployed URL
+						</a>
+					</li>
+					<li>
+						<a
+							href='https://github.com/billyhk/fire-frontend'
+							target='blank'
+							rel='noopener noreferrer'>
+							Github (front-end)
+						</a>
+					</li>
+					<li>
+						<a
+							href='https://github.com/andresaugusto/FIRE-back'
+							target='blank'
+							rel='noopener noreferrer'>
+							Github (back-end)
+						</a>
+					</li>
+				</section>
+
+				<section>
+					<div className='project-sublist-container'>
 						<li>Description:</li>
 						<ul className='project-sublist'>
-							<li>React/CSS/Bootstrap-React</li>
-							<li>Python/Django/PostgresSQL</li>
-							<li>Authentication with JWT</li>
+							<li>MERN stack</li>
+							<li>Unit Testing with Jest, Enzyme, Mocha, and Chai</li>
+							<li>Responsive mobile-first design</li>
 						</ul>
-					</section>
-				</ul>
-			</div>
-			<h2 className='about-heading2'>f i r e</h2>
+					</div>
+				</section>
+			</ul>
+		</div> */}
 
-			<div className='project-container'>
-				<img alt='fire-home' src={fire} width='100%' />
-				<ul className='project-description-list'>
-					<section>
+		<h2 className='about-heading2'>Recipe Searcher</h2>
+		<div className='project-container'>
+			{/* <img alt='statements-financial-statement' src={recipes} width='100%' /> */}{' '}
+			<Gallery images={GalleryImages['recipe']} enableImageSelection={false} />{' '}
+			<ul className='project-description-list'>
+				<section>
+					<li>
+						<a
+							href='https://recipe-searcher-2020.herokuapp.com'
+							target='_blank'
+							rel='noopener noreferrer'>
+							Deployed URL
+						</a>
+					</li>
+					<li>
+						<a
+							href='https://github.com/billyhk/recipe-app'
+							target='blank'
+							rel='noopener noreferrer'>
+							Github
+						</a>
+					</li>
+				</section>
+				<section>
+					<li>Description:</li>
+					<ul className='project-sublist'>
+						<li>Built with React Hooks</li>
 						<li>
+							Utilized 3rd Party API:{' '}
 							<a
-								href='https://fire-explorer-frontend.herokuapp.com'
+								href='https://spoonacular.com/food-api'
 								target='blank'
 								rel='noopener noreferrer'>
-								Deployed URL
+								Spoonacular
 							</a>
 						</li>
-						<li>
-							<a
-								href='https://github.com/billyhk/fire-frontend'
-								target='blank'
-								rel='noopener noreferrer'>
-								Github (front-end)
-							</a>
-						</li>
-						<li>
-							<a
-								href='https://github.com/andresaugusto/FIRE-back'
-								target='blank'
-								rel='noopener noreferrer'>
-								Github (back-end)
-							</a>
-						</li>
-					</section>
+						<li>Built with React-Router and Bootstrap stylings</li>
+					</ul>
+				</section>
+			</ul>
+		</div>
 
-					<section>
-						<div className='project-sublist-container'>
-							<li>Description:</li>
-							<ul className='project-sublist'>
-								<li>MERN stack</li>
-								<li>Unit Testing with Jest, Enzyme, Mocha, and Chai</li>
-								<li>Responsive mobile-first design</li>
-							</ul>
-						</div>
-					</section>
-				</ul>
-			</div>
-
-			<h2 className='about-heading2'>Recipe Searcher</h2>
-			<div className='project-container'>
-				<img alt='statements-financial-statement' src={recipes} width='100%' />
-				<ul className='project-description-list'>
-					<section>
-						<li>
-							<a
-								href='https://recipe-searcher-2020.herokuapp.com'
-								target='_blank'
-								rel='noopener noreferrer'>
-								Deployed URL
-							</a>
-						</li>
-						<li>
-							<a
-								href='https://github.com/billyhk/recipe-app'
-								target='blank'
-								rel='noopener noreferrer'>
-								Github
-							</a>
-						</li>
-					</section>
-					<section>
-						<li>Description:</li>
-						<ul className='project-sublist'>
-							<li>Built with React Hooks</li>
-							<li>
-								Utilized 3rd Party API:{' '}
-								<a
-									href='https://spoonacular.com/food-api'
-									target='blank'
-									rel='noopener noreferrer'>
-									Spoonacular
-								</a>
-							</li>
-							<li>Built with React-Router and Bootstrap stylings</li>
-						</ul>
-					</section>
-				</ul>
-			</div>
-
-			<h2 className='about-heading2'>Simon</h2>
-			<div className='project-container'>
-				<img alt='statements-financial-statement' src={simon} width='100%' />
-				<ul className='project-description-list'>
-					<section>
-						<li>
-							<a
-								href='https://simon-game-2020.herokuapp.com'
-								target='blank'
-								rel='noopener noreferrer'>
-								Deployed URL
-							</a>
-						</li>
-						<li>
-							<a
-								href='https://github.com/billyhk/simon-game'
-								target='blank'
-								rel='noopener noreferrer'>
-								Github
-							</a>
-						</li>
-					</section>
-					<section>
-						<li>Description:</li>
-						<ul className='project-sublist'>
-							<li>HTML5/CSS/Javacsript</li>
-							<li>Automated game engine</li>
-							<li>Sounds composed in Logic Pro / Audacity</li>
-						</ul>
-					</section>
-				</ul>
-			</div>
-		</main>
+		<h2 className='about-heading2'>Simon</h2>
+		<div className='project-container'>
+			{/* <img alt='statements-financial-statement' src={simon} width='100%' /> */}{' '}
+			<Gallery images={GalleryImages['simon']} enableImageSelection={false} />{' '}
+			<ul className='project-description-list'>
+				<section>
+					<li>
+						<a
+							href='https://simon-game-2020.herokuapp.com'
+							target='blank'
+							rel='noopener noreferrer'>
+							Deployed URL
+						</a>
+					</li>
+					<li>
+						<a
+							href='https://github.com/billyhk/simon-game'
+							target='blank'
+							rel='noopener noreferrer'>
+							Github
+						</a>
+					</li>
+				</section>
+				<section>
+					<li>Description:</li>
+					<ul className='project-sublist'>
+						<li>HTML5/CSS/Javacsript</li>
+						<li>Automated game engine</li>
+						<li>Sounds composed in Logic Pro / Audacity</li>
+					</ul>
+				</section>
+			</ul>
+		</div>
+	</main>
 );
 
 export default Projects;
