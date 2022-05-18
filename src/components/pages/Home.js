ce1c474be2186256c4c432ef8840149e97eef1d7
Billy Kaufman <billyhkaufman@gmail.com>
2022-05-17 22:46:17 -0400

    Added greeting to landing card

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Home = () => (
   <main className='home-main'>
     <div className='home-title'>
+      <p>Hi, I'm</p>
       <h1>Billy Kaufman</h1>
       <h2>Front-End Software Engineer</h2>
     </div>
