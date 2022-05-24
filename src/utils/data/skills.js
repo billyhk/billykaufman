541fc2deb853cd901f78d2ae1b5e3713023b6a0c
Billy Kaufman <billyhkaufman@gmail.com>
2022-05-24 15:09:24 -0400

    Added the rest of skill cards copy

--- src/utils/data/skills.js
+++ src/utils/data/skills.js
@@ -9,16 +9,37 @@ import {
 export const skillsData = [
   {
     Logo: <JsLogo width='100%' />,
-    heading: '',
-    body: '',
-  },
-  {
-    Logo: <TailwindLogo width='100%' />,
+    heading: 'JavaScript / TypeScript',
+    bodyList: [
+      'ReactJS / NextJS',
+      'Redux / Context',
+      'ES6 / ES2020+',
+      'Libraries: Formik/Yup, MaterialUI, React Table',
+    ],
   },
   {
     Logo: <GatsbyLogo width='100%' />,
+    heading: 'Headless CMS',
+    bodyList: [
+      'GatsbyJS',
+      'Wordpress',
+      'Custom Post Types and Advanced Custom Fields (ACFs)',
+      'Templating and Dynamic Routing',
+    ],
   },
   {
-    Logo: <Ellipses width='100%' className='skills-ellipses-icon'/>,
+    Logo: <TailwindLogo width='100%' />,
+    heading: 'Styling & Animation',
+    bodyList: [
+      'SVG animations',
+      'CSS / SCSS',
+      'PostCSS / TailwindCSS',
+      'Implementation of modern design trends',
+    ],
+  },
+  {
+    Logo: <Ellipses width='100%' className='skills-ellipses-icon' />,
+    heading: 'Other...',
+    bodyList: ['HTML / JSX', 'Shopify Liquid', 'GraphQL', 'Deployment via AWS'],
   },
 ]
