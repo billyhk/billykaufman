7dc28dc5d67d8d37605691b72ece6a042ac5ae41
Billy Kaufman <billyhkaufman@gmail.com>
2022-05-26 09:14:33 -0400

    Removed trailing slashes

--- src/utils/socialIcons.js
+++ src/utils/socialIcons.js
@@ -5,13 +5,12 @@ export const socialIcons = [
   {
     icon: (props) => <FaGithub {...props} />,
     href: 'https://github.com/billyhk/billykaufman',
-    // href: 'https://github.com/billyhk',
-    displayName: 'github.com/billyhk/',
+    displayName: 'github.com/billyhk',
   },
   {
     icon: (props) => <FaLinkedin {...props} />,
-    href: 'https://www.linkedin.com/in/williamhkaufman/',
-    displayName: '/in/williamhkaufman/',
+    href: 'https://www.linkedin.com/in/williamhkaufman',
+    displayName: '/in/williamhkaufman',
   },
   {
     icon: (props) => <FaEnvelope {...props} />,
