c644b008b21bcbab89183a199aba72a7a6121cc3
Billy Kaufman <billyhkaufman@gmail.com>
2022-05-28 19:12:16 -0400

    Added a project to the projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,16 +1,103 @@
 import React, { Fragment, useState } from 'react'
 import { NavToTop } from '../atoms'
+import { Carousel } from 'react-responsive-carousel'
+
+import collective1 from '../../component-images/project-images/collective/01 login.png'
+import collective2 from '../../component-images/project-images/collective/02a dashboard.png'
+import collective3 from '../../component-images/project-images/collective/02b company-details.png'
+import collective4 from '../../component-images/project-images/collective/3 users can have vendors that receive orders, or only use the platform to place orders -- filtered-vendors-list.png'
+import collective5 from '../../component-images/project-images/collective/04a create requisition for placing orders with other vendors.png'
+import collective6 from '../../component-images/project-images/collective/04b requisitions are broken down into orders.png'
+import collective7 from '../../component-images/project-images/collective/04c order-details.png'
+import collective8 from '../../component-images/project-images/collective/04d send message to selected vendors on an order.png'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+
 import '../../styles/pages/projects.css'
 
-const Projects = ({ referenceEl }) => {
-  // const [currentProjectSelected, setCurrentProjectSelected] = useState('')
+const projectsData = {
+  // sebpo: {
+  //   title: '',
+  //   client: '',
+  //   technologies: [''],
+  //   description: '',
+  //   images: [''],
+  // },
+  the_collective: {
+    title: 'The Collective',
+    client: 'G&G Holdings',
+    technologies: [
+      'ReactJS',
+      'ContextAPI',
+      'TailwindCSS',
+      'Formik/Yup',
+      'React-Table',
+    ],
+    description:
+      'This web application served as a purchasing agent for the construction industry. The platform can be used to create vendors that can receive orders, or to only place orders to vendors created by other users.',
+    images: [
+      collective1,
+      collective2,
+      collective3,
+      collective4,
+      collective5,
+      collective6,
+      collective7,
+      collective8,
+    ],
+  },
+}
+
+const Projects = () => {
+  const [currentProjectSelected, setCurrentProjectSelected] =
+    useState('the_collective')
 
   return (
-    <Fragment>
-      <NavToTop referenceEl={referenceEl} />
+    <main className='projects-main'>
+      {/* Heading */}
 
-      <main className='projects-main'></main>
-    </Fragment>
+      <div className='projects-header'>
+        <h1>My Past Projects</h1>
+        <h4>
+          Here are some projects I've worked on as a professional front-end
+          web-dev
+        </h4>
+      </div>
+
+      {/* PROJECT SELECTOR (dropdown menu) */}
+
+      <div className='projects__data--container'>
+        <div className='projects__carousel--container'>
+          <Carousel showArrows emulateTouch showStatus={false} autoPlay>
+            {projectsData[currentProjectSelected].images.map((image) => (
+              <img alt='' src={image} />
+            ))}
+          </Carousel>
+        </div>
+
+        {/* Glassmorphic card */}
+        <div className='projects__info-card'>
+          <h1>{projectsData[currentProjectSelected].title}</h1>
+          <div>
+            <h4>Client:</h4>
+            <p>{projectsData[currentProjectSelected].client}</p>
+          </div>
+          <div>
+            <h4>Technologies:</h4>
+            <div>
+              {projectsData[currentProjectSelected].technologies.map(
+                (technology) => (
+                  <p>{technology}</p>
+                )
+              )}
+            </div>
+          </div>
+          <div>
+            <h4>Description:</h4>
+            <p>{projectsData[currentProjectSelected].description}</p>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
