4f731364f9f9a1c9853867dc6babd21d20193549
Billy Kaufman <billyhkaufman@gmail.com>
2022-05-30 12:08:07 -0400

    Added svg to projects header; projects page styling updates

---
+++ src/components/atoms/svg/CgTemplate.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CgTemplate = ({ width = 24, color = '#000', ...props }) => (
+  <svg width={width} viewBox='0 0 24 24' fill='none' {...props}>
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M3 3V9H21V3H3ZM19 5H5V7H19V5Z'
+      fill={color}
+    />
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M3 11V21H11V11H3ZM9 13H5V19H9V13Z'
+      fill={color}
+    />
+    <path d='M21 11H13V13H21V11Z' fill={color} />
+    <path d='M13 15H21V17H13V15Z' fill={color} />
+    <path d='M21 19H13V21H21V19Z' fill={color} />
+  </svg>
+)
+export default CgTemplate
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,7 +4,8 @@ import { DropdownGlassmorphism } from '../molecules/Input/Dropdown'
 import { Carousel } from 'react-responsive-carousel'
 import { ArrowNext, ArrowPrev, Indicator, NavToTop } from '../atoms'
 import { ProjectCard } from '../molecules/Card'
-import { TemplateIcon } from '@heroicons/react/outline'
+// import { TemplateIcon } from '@heroicons/react/outline'
+import { CgTemplate } from '../atoms/svg'
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import '../../styles/pages/projects.css'
@@ -13,6 +14,15 @@ const staticCopy = {
   pageHeader: 'My Past Projects',
   subheading:
     "Here are some projects I've worked on as a professional front-end web-dev",
+  disclaimer: (
+    <span>
+      (Please be aware that many of these projects are still in development with
+      code that is <em>proprietary</em>, and therefore{' '}
+      <span style={{ fontFamily: 'monospace' }}>CLASSIFIED</span>. However, for
+      my non-proprietary words included here, feel free to check out their source
+      and/or code staging site.)
+    </span>
+  ),
 }
 
 const carouselProps = {
@@ -39,9 +49,14 @@ const Projects = ({ referenceEl }) => {
 
       <main className='projects-main'>
         <div className='projects-header'>
-          <TemplateIcon style={{ width: '4rem' }} />
+          <CgTemplate
+            width={100}
+            color='rgba(93, 0, 137, 1)'
+            style={{ transform: 'rotate(25deg)' }}
+          />
           <h1>{staticCopy.pageHeader}</h1>
           <h4>{staticCopy.subheading}</h4>
+          {/* <p style={{width: '50%'}}>{staticCopy.disclaimer}</p> */}
         </div>
 
         <div className='project-selector__container ease-in'>
