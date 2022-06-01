805fcc3005a029e4b111f332304de6e65c592f37
Billy Kaufman <billyhkaufman@gmail.com>
2022-05-31 21:58:02 -0400

    Removed unnused prop

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ function App() {
               <Route
                 exact
                 path={path}
-                referenceEl={ref}
                 component={() => <Component referenceEl={ref} />}
               />
             // </Suspense>
