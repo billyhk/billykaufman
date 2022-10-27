0357993fe9f53b6789c7e85d89d1cedc4675f5d8
Billy Kaufman <billyhkaufman@gmail.com>
2022-10-27 00:27:49 -0400

    Updated resume

--- src/utils/data/remoteAssets.js
+++ src/utils/data/remoteAssets.js
@@ -1,5 +1,5 @@
 export const REMOTE_ASSETS = {
   resume:
-    'https://drive.google.com/file/d/1yGo0vnSseWiHim6qI8xHspW19s5TB5cX/view?usp=sharing',
+    'https://drive.google.com/file/d/18d6_chvv2gb6XwtpS2Vk8xvuE-MT-jux/view?usp=sharing',
   projects: {},
 }
