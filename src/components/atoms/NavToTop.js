59124d8a39177e062db54e5a4967c71710ded1b7
Billy Kaufman <billyhkaufman@gmail.com>
2022-12-07 17:33:17 -0500

    Nav to Top smooth to fix mobile glitch when scrollToTop fires while scroll-y is moving

--- src/components/atoms/NavToTop.js
+++ src/components/atoms/NavToTop.js
@@ -5,7 +5,9 @@ export default function NavToTop({ referenceEl }) {
   return (
     <div
       className='nav-to-top'
-      onClick={() => referenceEl.current?.scrollTo({ top: 0 })}>
+      onClick={() =>
+        referenceEl.current?.scrollTo({ top: 0, behavior: 'smooth' })
+      }>
       <Arrow />
     </div>
   )
