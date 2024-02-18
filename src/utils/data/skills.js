7174085de2f6b74fc182e889dae2745353636034
Billy Kaufman <billy.kaufman@concertiv.com>
2024-02-18 17:26:15 -0500

    Updated projects and some other copy as well

--- src/utils/data/skills.js
+++ src/utils/data/skills.js
@@ -1,49 +1,50 @@
-import React from 'react'
 import {
-  TsLogo,
-  TailwindLogo,
-  GatsbyLogo,
   Ellipses,
-} from '../../components/atoms/svg'
+  GatsbyLogo,
+  TailwindLogo,
+  TsLogo,
+} from "../../components/atoms/svg";
+
+import React from "react";
 
 export const skillsData = [
   {
-    Logo: <TsLogo width='100%' />,
-    heading: 'TypeScript',
+    Logo: <TsLogo width="100%" />,
+    heading: "TypeScript",
     bodyList: [
-      'Object-Oriented JavaScript!',
-      'Typesafe, API-Driven, React Application Development',
-      'Unit Testing with Jest & Enzyme',
+      "Strongly Typed JavaScript!",
+      "Typesafe, API-Driven, React Application Development",
+      "Unit Testing with Jest & Enzyme",
       `ES6 / ES${new Date().getFullYear()}+`,
     ],
   },
   {
-    Logo: <TailwindLogo width='100%' />,
-    heading: 'Styling & Animation',
+    Logo: <TailwindLogo width="100%" />,
+    heading: "Styling & Animation",
     bodyList: [
-      'Advanced CSS Processing',
-      'CSS-Modules | TailwindCSS | SCSS | CSS-in-JS',
-      'Animations with Emotion, Framer-Motion, and GSAP',
-      'Implementation of Modern Design Trends and Morphisms',
+      "Advanced CSS Processing",
+      "CSS-Modules | TailwindCSS | SCSS | CSS-in-JS",
+      "Animations with Emotion, Framer-Motion, and GSAP",
+      "Implementation of Modern Design Trends and -morphisms",
     ],
   },
   {
-    Logo: <GatsbyLogo width='100%' />,
-    heading: 'Headless CMS',
+    Logo: <GatsbyLogo width="100%" />,
+    heading: "Headless CMS",
     bodyList: [
-      'Wordpress-Templating with GatsbyJS & GraphQL',
-      'Source Data from Multiple APIs',
-      'No-Code Content-Management via Custom Post Types and Advanced Custom Fields',
+      "Wordpress-Templating with GatsbyJS & GraphQL",
+      "Source Data from Multiple APIs",
+      "No-Code content-management for clients via Custom Post Types and Advanced Custom Fields",
     ],
   },
 
   {
-    Logo: <Ellipses width='100%' className='skills-ellipses-icon' />,
-    heading: 'Other...',
+    Logo: <Ellipses width="100%" className="skills-ellipses-icon" />,
+    heading: "Other...",
     bodyList: [
-      'CMS Development with Wordpress, Shopify, Wix, Squarespace, Etc.',
-      'SVG Optimization and UI Integration',
-      'Deployment with Amazon Web Services',
+      "CMS Development with Wordpress, Shopify, Wix, Squarespace, Etc.",
+      "SVGs: Optization, Integration, Animation",
+      "Deployment with Amazon Web Services",
     ],
   },
-]
+];
