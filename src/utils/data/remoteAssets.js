62854b86129cb43ae7d4c397a63b76fe0cccf146
Billy Kaufman <billy.kaufman@concertiv.com>
2024-02-19 10:33:41 -0500

    Updated resume link again

--- src/utils/data/remoteAssets.js
+++ src/utils/data/remoteAssets.js
@@ -1,5 +1,5 @@
 export const REMOTE_ASSETS = {
   resume:
-    "https://drive.google.com/file/d/1odYBrTlSqACLC3UP0loDAjaE4_KNciaT/view?usp=sharing",
+    "https://drive.google.com/file/d/1macj8-xGW42J57zly6Akx9xJFUfn-mwg/view?usp=sharing",
   projects: {},
 };
